14e62415e95e11ffe1741644b2a55206b2b39142
Itxchii <yanis421018@gmail.com>
2024-03-30 21:44:57 +0100

    --fix bug date_heure

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+ 
 # import matplotlib.pyplot as plt
 import plotly.express as px
 from src.fetch_data import load_data_from_lag_to_today
@@ -9,35 +9,35 @@ import logging
 import os
 import glob
 from datetime import datetime
-
-
+ 
+ 
 def remove_data(df: pd.DataFrame, last_n_samples: int = 4*3):
-
+ 
     # df: pd.DataFrame = pd.read_csv(fic_export_data)
     return df.iloc[:-last_n_samples]
     # df.to_csv(fic_export_data, index=False)
 logging.basicConfig(level=logging.INFO)
-
-
+ 
+ 
 LAG_N_DAYS: int = 7
-
+ 
 # * INIT REPO FOR DATA
 os.makedirs("data/raw/", exist_ok=True)
 os.makedirs("data/interim/", exist_ok=True)
-
+ 
 # * remove outdated json files
 for file_path in glob.glob("data/raw/*json"):
     try:
         os.remove(file_path)
     except FileNotFoundError as e:
         logging.warning(e)
-
+ 
 # plt.switch_backend("TkAgg")
-
+ 
 # Title for your app
 st.title("Data Visualization App")
-
-
+ 
+ 
 # Load data from CSV
 @st.cache_data(
     ttl=15 * 60
@@ -49,20 +49,20 @@ def load_data(lag_days: int):
         fic_export_data, parse_dates=[col_date]
     )  # Adjust 'DateColumn' to your date column name*
     return data
-
-
+ 
+ 
 # Assuming your CSV is named 'data.csv' and is in the same directory as your app.py
 df = load_data(LAG_N_DAYS)
 df = remove_data(df, last_n_samples=4*24)
-
+ 
 # Creating a line chart
 st.subheader("Line Chart of Numerical Data Over Time")
-
+ 
 # Select the numerical column to plot
 # This lets the user select a column if there are multiple numerical columns available
 # numerical_column = st.selectbox('Select the column to visualize:', df.select_dtypes(include=['float', 'int']).columns)
 numerical_column = col_donnees
-
+ 
 # ! Matplotlib - Create the plot
 # fig, ax = plt.subplots()
 # ax.plot(df[col_date], df[numerical_column])  # Adjust 'DateColumn' to your date column name
@@ -71,22 +71,22 @@ numerical_column = col_donnees
 # ax.set_title(f'Time Series Plot of {numerical_column}')
 # # Display the plot
 # st.pyplot(fig)
-
+ 
 now = datetime.now()
 now_aware = now.replace(microsecond=0).isoformat() + '+00:00'
 formatted_time = datetime.fromisoformat(now_aware)
 last = df.iloc[-1][col_date]
-
+ 
 diff = formatted_time - last
-
-
+ 
+ 
 # ! Plotly
 # Create interactive line chart using Plotly
 fig = px.line(df, x=col_date, y=col_donnees, title="Consommation en fonction du temps")
 st.plotly_chart(fig)
-
+ 
 st.subheader(f'Temps manquants: {diff}')
-
+ 
 day_of_week_map = {
     0: 'Monday',
     1: 'Tuesday',
@@ -97,6 +97,6 @@ day_of_week_map = {
     6: 'Sunday'
 }
 df['day_of_week'] = df[col_date].dt.dayofweek.map(day_of_week_map)
-
+ 
 fig2 = px.bar(df.groupby('day_of_week')[col_donnees].mean().reset_index(), x='day_of_week', y=col_donnees, title="Moyenne de la consommation des jours de la semaine")
 st.plotly_chart(fig2)
